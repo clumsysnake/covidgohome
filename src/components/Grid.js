78ed2f0e817aa28e9913f9ee5fb83182ccbd90bc
Ryan Ziegler <ryan@symbolforce.com>
2020-03-19 04:42:12 -0700

    first

---
+++ src/components/Grid.js
@@ -0,0 +1,80 @@
+import React from "react"
+import _ from 'lodash'
+
+import USState from './USState.js'
+
+const COVIDTRACKING_STATESDAILY_URL = "https://covidtracking.com/api/states/daily"
+const COVIDTRACKING_USDAILY_URL = "https://covidtracking.com/api/us/daily"
+
+const DEBUG_MAX_STATES = 1000 //CRZ: set lower to limit # of states displayed
+// const DEBUG_MAX_STATES = 5 
+
+class Grid extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {statesDaily: null, usDaily: null}
+
+    this.fetchJson(COVIDTRACKING_STATESDAILY_URL, 'statesDaily')
+    this.fetchJson(COVIDTRACKING_USDAILY_URL, 'usDaily')
+  }
+
+  fetchJson(url, stateKey) {
+    let t = this;
+
+    let oReq = new XMLHttpRequest();
+    oReq.onload = function(e) {
+      t.setState({[stateKey]: e.target.response});
+    }
+    oReq.open("GET", url);
+    oReq.responseType = "json";
+    oReq.send();
+  }
+
+  decorateTimeSeries(entries) {
+    return entries.map((e, idx, a) => {
+      e.posNeg = e.positive + e.negative //where total is neg + pos + pending
+      e.posNegChange = (idx > 0) ? e.posNeg - a[idx-1].posNeg : null
+      e.posChange = (idx > 0) ? e.positive - a[idx-1].positive : null
+      e.posPercToday = (idx > 0) ? (e.posChange / e.posNegChange) * 100 : null
+
+      //regularize broken data
+      if(e.negative === null && idx > 0) { e.negative = a[idx-1].negative }
+
+      return e
+    })
+  }
+
+  render() {
+    let groups = _.groupBy(this.state.statesDaily, 'state')
+
+    let globalXDates = _.uniq(
+      Object.entries(groups).flatMap((pair) => {
+        return pair[1].map(date => date)
+      })
+    ).sort()
+    let globalXDomain = [globalXDates[0], globalXDates[globalXDates.length-1]]
+    // console.log(globalXDomain)
+
+    let states = Object.entries(groups)
+      .slice(0, DEBUG_MAX_STATES)
+      .sort() // alphabetic sort by state code
+
+    console.log(states);
+
+    let stateComps = states.map((pair) => {
+      let entries = this.decorateTimeSeries(pair[1].reverse() )
+      // console.log(this.decorateTimeSeries(pair[1]))
+      return <USState key={pair[0]} entries={entries} globalXDomain={globalXDomain}/>
+    })
+
+
+    return <div>
+      {stateComps}
+    </div>
+  }
+}
+
+Grid.propTypes = {}
+Grid.defaultProps = {}
+export default Grid
---
+++ src/components/USState.js
@@ -0,0 +1,77 @@
+import React from "react"
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import "./USState.css"
+
+const PERCENTAGE_COLOR = '#882498'
+const COUNT_COLOR = '#3324d8'
+
+class USState extends React.Component {
+  stateCode() {
+    return this.props.entries[0].state
+  }
+
+  render() {
+    //CRZ: fix up data such that it displays nicely.
+    let data = this.props.entries.map((e, idx, a) => {
+      let flooredPosPercToday = Math.max(0, e.posPercToday)
+
+      e.displayPosPercToday = Number.parseFloat(flooredPosPercToday).toFixed(0)
+      e.displayDate = e.date.toString().slice(4, 6) + "-" + e.date.toString().slice(6, 8)
+
+      return e
+    })
+
+    return (
+      <div className="us-state">
+        <div className="state-code">{this.stateCode()}</div>
+        <LineChart width={600} height={300} data={data}
+                   margin={{ top: 10, right: 10, left: 10, bottom: 10 }}
+          >
+          <Line 
+            yAxisId="left"
+            type="monotone"
+            dataKey="posNegChange"
+            stroke={COUNT_COLOR}
+            strokeWidth={3}
+            isAnimationActive={false}
+            name="# Tests"
+          />
+          <Line
+            yAxisId="percentage"
+            type="monotone"
+            dataKey="displayPosPercToday"
+            stroke={PERCENTAGE_COLOR}
+            isAnimationActive={false}
+            name="% (+) Tests"
+          />
+          <XAxis dataKey="displayDate" domain={this.props.globalXDomain} />
+          <Tooltip />
+          <YAxis
+            yAxisId="left"
+            orientation="left"
+            type="number"
+            allowDataOverflow={false}
+            domain={[0,1000]}
+            tick={{stroke: COUNT_COLOR}}
+          />
+          <YAxis
+            yAxisId="percentage"
+            orientation="right"
+            type="number"
+            allowDataOverflow={false}
+            domain={[0, 100]}
+            tick={{stroke: PERCENTAGE_COLOR}}
+          />
+          <Legend />
+        </LineChart>
+      </div>
+    )
+  }
+}
+
+USState.propTypes = {
+  //entries, array of covidtracking rows
+  //globalXDomain
+}
+USState.defaultProps = {}
+export default USState
