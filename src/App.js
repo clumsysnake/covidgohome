12e7e2111f5f2c7af94ab4dc0e63fc868b9d2f55
Ryan Ziegler <ryan@symbolforce.com>
2020-03-24 21:33:45 -0700

    fixing header

--- src/App.js
+++ src/App.js
@@ -27,43 +27,52 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <em>...go back in your bat hole! </em> 
-          (pulled from <a href="https://covidtracking.com/">https://covidtracking.com/</a>)
-          (ryan at ryan dawt org)
-        </header>
-        <div className="filters">
-          <div className="sort">
-            sort:
-            {this.state.sort === "most-tests" ? " most tests " : <button onClick={this.clickedSortMostTests}>most tests</button>}
-            |
-            {this.state.sort === "percent-confirmed" ? " percent-confirmed " : <button onClick={this.clickedSortPercentConfirmed}>percent-confirmed</button>}
-          </div>
+        <div className="top">
+          <header className="App-header">
+            <em>...go back in your bat hole! </em> 
+            (pulled from <a href="https://covidtracking.com/">https://covidtracking.com/</a>)
+            (ryan at ryan dawt org)
+          </header>
+          <div className="filters">
+            <div className="sort">
+              sort:
+              {this.state.sort === "most-tests" ? " most tests " : <button onClick={this.clickedSortMostTests}>most tests</button>}
+              |
+              {this.state.sort === "percent-confirmed" ? " percent-confirmed " : <button onClick={this.clickedSortPercentConfirmed}>percent-confirmed</button>}
+            </div>
 
-          <div className="group">
-            group:
-            {this.state.group === "none" ? " none " : <button onClick={this.clickedGroupNone}>none</button>} 
-            |
-            {this.state.group === "region" ? " region " : <button onClick={this.clickedGroupRegion}>region</button>} 
-          </div>
-          
-          <div className="aggregate">
-            aggregate:
-            {this.state.aggregate === "none" ? " none " : <button onClick={this.clickedAggregateNone}>none</button>}
-            |
-            {this.state.aggregate === "region" ? " region " : <button onClick={this.clickedAggregateRegion}>region</button>}
-            |
-            {this.state.group === "country" ? " country " : <button onClick={this.clickedAggregateCountry}>country</button>} 
-          </div>
+            <div className="group">
+              group:
+              {this.state.group === "none" ? " none " : <button onClick={this.clickedGroupNone}>none</button>} 
+              |
+              {this.state.group === "region" ? " region " : <button onClick={this.clickedGroupRegion}>region</button>} 
+            </div>
+            
+            <div className="aggregate">
+              aggregate:
+              {this.state.aggregate === "none" ? " none " : <button onClick={this.clickedAggregateNone}>none</button>}
+              |
+              {this.state.aggregate === "region" ? " region " : <button onClick={this.clickedAggregateRegion}>region</button>}
+              |
+              {this.state.group === "country" ? " country " : <button onClick={this.clickedAggregateCountry}>country</button>} 
+            </div>
 
-          <div className="chart-type">
-            type:
-            {this.state.chartType === "daily" ? " daily " : <button onClick={this.clickedChartTypeDaily}>daily</button>}
-            |
-            {this.state.chartType === "cumulative" ? " cumulative " : <button onClick={this.clickedChartTypeCumulative}>cumulative</button>}
+            <div className="chart-type">
+              type:
+              {this.state.chartType === "daily" ? " daily " : <button onClick={this.clickedChartTypeDaily}>daily</button>}
+              |
+              {this.state.chartType === "cumulative" ? " cumulative " : <button onClick={this.clickedChartTypeCumulative}>cumulative</button>}
+            </div>
           </div>
         </div>
-        <Grid sort={this.state.sort} aggregate={this.state.aggregate} group={this.state.group} chartType={this.state.chartType}/>
+        <div className="bottom">
+          <Grid
+            sort={this.state.sort}
+            aggregate={this.state.aggregate}
+            group={this.state.group}
+            chartType={this.state.chartType}
+          />
+        </div>
       </div>
     )
   }
