e0577411e5422bf4f5040709748d22b383c3ab8b
Ryan Ziegler <ryan@symbolforce.com>
2020-03-27 07:00:03 -0700

    stateModel uses census store to give itself a name

--- src/models/StateModel.js
+++ src/models/StateModel.js
@@ -1,5 +1,6 @@
 import RegionModel from './RegionModel'
 import AreaModel from './AreaModel'
+import { stateNameForAbbrev } from '../stores/CensusStore'
 
 let allModels = []
 
@@ -7,7 +8,19 @@ class StateModel extends AreaModel {
   constructor(props) {
     super(props)
 
+    this.abbreviation = props.name
+    this.name = stateNameForAbbrev(this.abbreviation)
+
+    //TODO: prevent two states with same name or abbreviation
     allModels.push(this)
+  }
+
+  static findByAbbreviation(abbrev) {
+    return StateModel.all.find(s => s.abbreviation === abbrev)
+  }
+
+  static findByName(name) {
+    return StateModel.all.find(s => s.name === name)
   }
 
   static get all() {
