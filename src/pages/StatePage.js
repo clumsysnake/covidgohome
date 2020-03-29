a968623fc1a894c55947809edad55d19587014ff
Ryan Ziegler <ryan@symbolforce.com>
2020-03-28 21:36:14 -0700

    adding dead per million

--- src/pages/StatePage.js
+++ src/pages/StatePage.js
@@ -4,13 +4,16 @@ import DailyChart from '../components/DailyChart.js'
 import CumulativeChart from '../components/CumulativeChart.js'
 import './StatePage.css'
 import { numberWithCommas, withPlaces } from '../helpers/chartHelpers.js'
+import _ from 'lodash'
 
 function StatePage(props) {
   let state = props.state
 
   if(!state) { return <div>...loading</div> }
 
-  let scaledSeries = state.scaledToPercentage()
+  let scaledPercentage = state.scaledToPercentage()
+  //TODO: this line has so much wrong with it... fix AreaModel
+  let deadPer1M = _.last(state.scaledPerMillion()).death
 
   return (
     <div className="state-page">
@@ -19,11 +22,11 @@ function StatePage(props) {
         <ul>
           <li>Population {numberWithCommas(state.population)}</li>
           <li>Confirmed: {state.totals.confirmed}</li>
-          <li>Currently Infected: Unknown (need recovered data)</li>
           <li>Attack Rate {withPlaces(state.totals.attackRate, 3)}%</li>
           <li>Case Fatality Ratio: {withPlaces(state.totals.cfrPercent, 2)}% (based on confirmed, not resolved)</li>
-          <li>Dead: {state.totals.dead}</li>
+          <li>Dead: {state.totals.dead} people, or {withPlaces(deadPer1M, 2)} per million</li>
           <li>Hospitalized: {state.totals.hospitalized || "Unknown"}</li>
+          <li>Currently Infected: Unknown (need recovered data)</li>
           <li>Recovered: Unknown</li>
           <li>Resolved: Unknown</li>
         </ul>
@@ -32,7 +35,7 @@ function StatePage(props) {
       <div className="charts">
         <DailyChart name="Daily Changes" series={state.entries} />
         <CumulativeChart name="Cumulative Test Breakdown"
-          series={scaledSeries} basis="percentage" yDomain={[0,100]}/>
+          series={scaledPercentage} basis="percentage" yDomain={[0,100]}/>
       </div>
     </div>
   )
