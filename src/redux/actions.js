dcb432185206d41bce3692c3106f06b829b6365b
Ryan Ziegler <ryan@symbolforce.com>
2020-04-01 06:50:24 -0700

    shove in NYC census population hack... not a real solution but it helps nyc blow the color scale less by giving it the proper population

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -85,6 +85,28 @@ export function johnsHopkinsFetchDailyReport() {
   };
 }
 
+//New York City extends over its 5 boroughs. I assume its borders are coterminous with the combined area of its 5 counties:
+  // Census data:
+  // ["005","12","7/1/2019 population estimate","33726.504271","1418207","Bronx County, New York","36","12","36","005"],
+  // ["061","12","7/1/2019 population estimate","71874.138732","1628706","New York County, New York","36","12","36","061"],
+  // ["081","12","7/1/2019 population estimate","20719.741247","2253858","Queens County, New York","36","12","36","081"],
+  // ["085","12","7/1/2019 population estimate","8277.8254914","476143","Richmond County, New York","36","12","36","085"],
+  // ["047","12","7/1/2019 population estimate","36901.373131","2559903","Kings County, New York","36","12","36","047"],
+//JH data, where it incorrectly lists new york city as FIPS 36061.
+  //36061,New York City,New York,US,2020-03-31 23:43:56,40.76727260000001,-73.97152637,43119,932,0,0,"New York City, New York, US"
+//CRZ: hackily, very hackily, give new york county's population as that of new york city's, to stop distorting the county maps.
+//     JH gives us badly formatted data, aggregated by city not county, and i need to support a more complex data model.
+//     this will fix color scaling issues for now.
+function censusDataNYCHack() {
+  const nycFIPS = ["36005", "36061", "36081", "36085", "36047"]
+
+  return {
+    // density:
+    // name:
+    population: nycFIPS.map(f => censusDataForFips(f).population).reduce((a, p) => a + p)
+  }
+}
+
 //CRZ: TODO: consider streaming.
 //CRZ: TODO: does active = positive - death - recovered? if so can just lazily calc it
 //CRZ: TODO: does last_update matter here?
@@ -97,7 +119,7 @@ function johnsHopkinsHandleDailyReport(csv, date) {
   })
 
   let counties = results.data.filter(res => res.Country_Region === "US" && res.FIPS).map(res => {
-    const censusData = censusDataForFips(res.FIPS)
+    const censusData = (res.FIPS === "36061") ? censusDataNYCHack() : censusDataForFips(res.FIPS)
 
     if(!censusData) { console.log(`couldn't census data for county ${res.Admin2}, FIPS ${res.FIPS}`) }
 
