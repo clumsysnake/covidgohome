8aea7476c3556ffceb08137c85127d6c2704c823
Ryan Ziegler <ryan@symbolforce.com>
2020-04-02 00:41:06 -0700

    updating about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,16 +5,17 @@ class AboutPage extends React.Component {
     return (
       <div className="page-about">
         <p>
+          Mission: Find the real trends in covid progression, focusing on the USA. 
+          Express no more certainty than the data warrants.
+        </p>
+        <p>
+          dreaming of a day when covid flies away - ryan at ryan dawt org
+        </p>
+        <p>
           Source: <a href="https://covidtracking.com/">COVID Tracking Project</a>
         </p>
         <p>
           Source: <a href="https://github.com/CSSEGISandData/COVID-19">Johns Hopkins github repo</a>
-        </p>
-        <p>
-          ...dreaming of a day when covid flies away
-        </p>
-        <p>
-          - ryan at ryan dawt org
         </p>
       </div>
     )
