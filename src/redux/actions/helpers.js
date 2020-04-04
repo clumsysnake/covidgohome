1a1077a0a3a125e6618c10ede30fca5c37f31874
Ryan Ziegler <ryan@symbolforce.com>
2020-04-04 16:15:23 -0700

    defaulting xhr headers to not cache

--- src/redux/actions/helpers.js
+++ src/redux/actions/helpers.js
@@ -3,6 +3,8 @@ export function fetchXhr(url, onload = null, onerror = null, responseType = "jso
   oReq.onload = onload
   oReq.onerror = onerror
   oReq.open("GET", url);
+  oReq.setRequestHeader("Pragma", "no-cache");
+  oReq.setRequestHeader("Cache-control", "no-cache");
   oReq.responseType = responseType;
   oReq.send();
 }
