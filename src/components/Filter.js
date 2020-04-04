1dd95eaefe40111f0d087895a3b8a7ccafd12a65
Ryan Ziegler <ryan@symbolforce.com>
2020-04-04 00:26:51 -0700

    restyling filters (still can’t get the cursor:pointer to trigger proper)

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,7 +4,7 @@ import './Filter.css'
 export default function(props) {
   //TODO: accept name=? or extra classNames?
   return <div className="filter-control">
-    <span className="filter-label">{(props.label) ? `${props.label}:` : null}</span>
+    <span className="filter-label">{(props.label) ? `${props.label} ` : null}</span>
     {props.options.map(option => filterOption(props.accessors, option))}
   </div>
 }
@@ -13,9 +13,11 @@ export default function(props) {
 const filterOption = function(accessors, option) {
   let [value, displayValue] = (typeof option === "string") ? [option, option] : [option[0], option[1]]
 
-  return <span key={value+displayValue} className="filter-option">
-    {(accessors[0] === value) ? displayValue : <button
-      onClick={() => accessors[1](value)}>{displayValue}
+  let isActive = accessors[0] === value
+
+  return <span key={value+displayValue} className={`filter-option ${isActive ? 'chosen': ''}`}>
+    {(isActive) ? <div className="inner-filter-option">displayValue</div> : <button
+      className="inner-filter-option" onClick={() => accessors[1](value)}>{displayValue}
     </button>}
   </span>
 }
